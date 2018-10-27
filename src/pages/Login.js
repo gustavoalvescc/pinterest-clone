465aeb7ea27bc08b5f57e7c99979757a6bb78910
gustavoalvescc@gmail.com <gustavoalvescc@gmail.com>
2018-10-27 11:01:04 -0300

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 export class Login extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  handleLogin(){
+    this.props.history.push("/home");
+  }
   render(){
     return (
-      <h1>Login</h1>
+      <div className="login-container">
+        <div className="input-container">
+          <label forhtml="user_login">User</label>
+          <input type="text" id="user_login" />
+        </div>
+        <div className="input-container">
+          <label forhtml="pass_login">Password</label>
+          <input type="password" id="pass_login" />
+        </div>
+        <button onClick={this.handleLogin}>Login</button>
+      </div>
     );
   }
 };
