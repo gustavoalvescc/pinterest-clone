dc71c8bd23332e3693fe4ac3683c341126b7c753
Roger <roger.sama@gmail.com>
2018-10-27 16:44:09 -0300

    Styling login and pin wall components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Header} from './components/Header';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app-wrapper">
         <Header />
         <Main />
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,6 +11,9 @@ export class Login extends React.Component{
   render(){
     return (
       <div className="login-container">
+        <div className="app-name">
+          PINTEREST-CLONE
+        </div>
         <div className="input-container">
           <label forhtml="user_login">User</label>
           <input type="text" id="user_login" />
@@ -19,7 +22,7 @@ export class Login extends React.Component{
           <label forhtml="pass_login">Password</label>
           <input type="password" id="pass_login" />
         </div>
-        <button onClick={this.handleLogin}>Login</button>
+        <a class="btn btn-raised btn-primary" onClick={this.handleLogin}>Login</a>
       </div>
     );
   }
